4ce660b1f4534f72ea76396947e34a24fe2d3287
saber <fatestaymq2@foxmail.com>
2018-05-09 18:32:57 +0800

    1.init 2.read xml

---
+++ slg/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace map
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlParaser pra = new XmlParaser("C:/Users/Shmily/Desktop/test3.xml");
+            CrustalBlockCollection testCollection = pra.GetAllCrustalBlock();
+        }
+    }
+}
---
+++ slg/XmlParaser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace map
+{
+    public class XmlParaser
+    {
+        public XmlParaser(String xmlPath)
+        {
+            //crustalBlockNodes = new List<CrustalBlock>();
+            CBCollection = new CrustalBlockCollection();
+            try
+            {
+                /*FileStream fs = new FileStream(xmlPath, FileMode.Open, FileAccess.Read);
+                XmlSerializer xs = new XmlSerializer(typeof(CrustalBlock));
+                CrustalBlock cb = xs.Deserialize(fs) as CrustalBlock;*/
+
+                FileStream fs = new FileStream(xmlPath, FileMode.Open, FileAccess.Read);
+                XmlSerializer xs = new XmlSerializer(typeof(CrustalBlockCollection));
+                CBCollection = xs.Deserialize(fs) as CrustalBlockCollection;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        //public List<CrustalBlock> GetAllCrustalBlock() {
+        //    return crustalBlockNodes;
+        //}
+
+        public CrustalBlockCollection GetAllCrustalBlock()
+        {
+            return CBCollection;
+        }
+
+        // private List<CrustalBlock> crustalBlockNodes;
+        private CrustalBlockCollection CBCollection;
+    }
+}
