1b661d2322af7d563868c1062c36c675e6a6f6f9
xiongermao <449001591@qq.com>
2018-06-05 23:30:54 +0800

    上传之前文档

---
+++ 设计文档/杂项文件/Code/Cellnature_table.cpp
@@ -0,0 +1,127 @@
+﻿#include "Cellnature_table.hpp"
+#include "tinyxml.h"
+#include "EeFileMemory.h"
+#include "EeFilePackage.h"
+
+CellnatureTableMgr* CellnatureTableMgr::m_pkCellnatureTableMgr = NULL;
+CellnatureTableMgr* CellnatureTableMgr::Get()
+{
+    if(m_pkCellnatureTableMgr == NULL)
+    {
+        m_pkCellnatureTableMgr = new CellnatureTableMgr();
+    }
+    return m_pkCellnatureTableMgr;
+}
+
+
+const std::map<unsigned int, CellnatureTable*>& CellnatureTableMgr::GetCellnatureTableMap()
+{
+    return m_kCellnatureTableMap;
+}
+
+TableResArray CellnatureTableMgr::GetCellnatureTableVec()
+{
+    TableResArray kRecVec;
+    for (std::map<unsigned int, CellnatureTable*>::iterator iMapItr = m_kCellnatureTableMap.begin(); iMapItr != m_kCellnatureTableMap.end(); ++iMapItr)
+    {
+        kRecVec.pushBack(iMapItr->second);
+    }
+
+    return kRecVec;
+}
+
+const CellnatureTable* CellnatureTableMgr::GetCellnatureTable(unsigned int iTypeID)
+{
+    std::map<unsigned int, CellnatureTable*>::iterator iter = m_kCellnatureTableMap.find(iTypeID);
+    if(iter != m_kCellnatureTableMap.end())
+    {
+        return iter->second;
+    }
+    return NULL;
+}
+
+CellnatureTableMgr::CellnatureTableMgr()
+{
+    Load();
+}
+
+CellnatureTableMgr::~CellnatureTableMgr()
+{
+    m_kCellnatureTableMap.clear();
+}
+
+bool CellnatureTableMgr::Load()
+{
+    std::string path = "Data/Table/Cellnature.xml";
+    FileMemory kMemory;
+    if(!FileLoader::LoadTableFile(path.c_str(),kMemory))
+    {
+        return false;
+    }
+
+    TiXmlDocument doc;
+    doc.Parse(kMemory.GetData());
+    if (doc.Error())
+    {
+        std::string err = path + "   " + std::string(doc.ErrorDesc());
+        // throw std::exception(err.c_str());
+        return false;
+    }
+
+    TiXmlElement* root = doc.FirstChildElement("root");
+    if (root == 0)
+    {
+        // throw std::exception("root is null!");
+        return false;
+    }
+
+    TiXmlElement* element = root->FirstChildElement("content");
+    while (element != 0)
+    {
+        CellnatureTable * row = new CellnatureTable();
+        FillData(row, element);
+        element = element->NextSiblingElement();
+    }
+
+    return true;
+}
+
+void CellnatureTableMgr::FillData(CellnatureTable* row, TiXmlElement* element)
+{
+    int int_value = 0;
+    std::string str_value = "";
+    double float_value = 0.0f;
+
+    element->Attribute("CellID", &int_value);
+    row->uiCellID = (unsigned int)int_value;
+    str_value = element->Attribute("CellName");
+    row->kCellName = str_value;
+    str_value = element->Attribute("CellSize");
+    row->kCellSize = str_value;
+    element->Attribute("PassEfficiency", &int_value);
+    row->uiPassEfficiency = (unsigned int)int_value;
+    element->Attribute("AtkAdd", &int_value);
+    row->uiAtkAdd = (unsigned int)int_value;
+    element->Attribute("DefAdd", &int_value);
+    row->uiDefAdd = (unsigned int)int_value;
+    element->Attribute("HitAdd", &int_value);
+    row->uiHitAdd = (unsigned int)int_value;
+    element->Attribute("EvAdd", &int_value);
+    row->uiEvAdd = (unsigned int)int_value;
+    element->Attribute("HPRecovery", &int_value);
+    row->uiHPRecovery = (unsigned int)int_value;
+    element->Attribute("AtkTime", &int_value);
+    row->uiAtkTime = (unsigned int)int_value;
+    element->Attribute("MotherCell", &int_value);
+    row->uiMotherCell = (unsigned int)int_value;
+    element->Attribute("MinCent", &float_value);
+    row->fMinCent = (float)float_value;
+    element->Attribute("MaxCent", &float_value);
+    row->fMaxCent = (float)float_value;
+    element->Attribute("MinDistance", &int_value);
+    row->uiMinDistance = (unsigned int)int_value;
+    element->Attribute("SameDistance", &int_value);
+    row->uiSameDistance = (unsigned int)int_value;
+
+    m_kCellnatureTableMap[row->uiCellID] = row;
+}
---
+++ 设计文档/杂项文件/Code/Cellnature_table.hpp
@@ -0,0 +1,48 @@
+﻿#pragma once
+#pragma execution_character_set("utf-8")
+
+#include <string>
+#include <map>
+#include "SeTableResBase.h"
+
+
+
+//  自动生成表结构
+struct CellnatureTable : SeTableResBase
+{
+    unsigned int               uiCellID                            ;   //  编号   
+    std::string                kCellName                           ;   //  地块名称   
+    std::string                kCellSize                           ;   //  地块尺寸   
+    unsigned int               uiPassEfficiency                    ;   //  通行效率   
+    unsigned int               uiAtkAdd                            ;   //  攻击加成   
+    unsigned int               uiDefAdd                            ;   //  防御加成   
+    unsigned int               uiHitAdd                            ;   //  命中加成   
+    unsigned int               uiEvAdd                             ;   //  闪避加成   
+    unsigned int               uiHPRecovery                        ;   //  生命回复   
+    unsigned int               uiAtkTime                           ;   //  破坏次数   
+    unsigned int               uiMotherCell                        ;   //  母地块   
+    float                      fMinCent                            ;   //  地块最小占比   
+    float                      fMaxCent                            ;   //  地块最大占比   
+    unsigned int               uiMinDistance                       ;   //  边界最小距离   
+    unsigned int               uiSameDistance                      ;   //  相同地块最小距离   
+};
+
+class TiXmlElement;
+class CellnatureTableMgr
+{
+public:
+    static CellnatureTableMgr* Get();
+    CellnatureTableMgr();
+    ~CellnatureTableMgr();
+
+    const CellnatureTable* GetCellnatureTable(unsigned int iTypeID);
+    const std::map<unsigned int, CellnatureTable*>& GetCellnatureTableMap();
+    TableResArray GetCellnatureTableVec();
+
+private:
+    bool    Load();
+    void    FillData(CellnatureTable* row, TiXmlElement* element);
+
+    std::map<unsigned int,CellnatureTable*>      m_kCellnatureTableMap;
+    static CellnatureTableMgr* m_pkCellnatureTableMgr;
+};
