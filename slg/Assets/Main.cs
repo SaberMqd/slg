4e24be4f6c7b3538b9ddf3e06840729c908b6094
saber <fatestaymq2@foxmail.com>
2018-06-19 11:39:40 +0800

    1.仅有一种角色。 可以配置属性。可以随便放着玩了

--- slg/Assets/Main.cs
+++ slg/Assets/Main.cs
@@ -8,17 +8,16 @@ public class Main : MonoBehaviour
 
     void Start()
     {
-        return;
         entityManager = World.Active.GetOrCreateManager<EntityManager>();
         var mapEntity = entityManager.CreateEntity(typeof(MapType));
         entityManager.SetComponentData<MapType>(mapEntity, new MapType { Type = 1 });
 
+        return;
         var archetype = entityManager.CreateArchetype(
             typeof(CreateCharaterData),
             typeof(CharacterCoordinate),
             typeof(CombatAttributes),
             typeof(BaseCharacterAttribute));
-
         var e1 = entityManager.CreateEntity(archetype);
         entityManager.SetComponentData(e1, new BaseCharacterAttribute { Lv = 1, XP = 0, Movement = 3, HP = 100, Power = 2, MP = 100, Skill = 1, Speed = 1, Lucky = 1, Defense = 1, MagicResistance = 1 });
         entityManager.SetComponentData(e1, new CombatAttributes { Attack = 1,HitRate = 1,Cri = 1, Dodge = 1, });
