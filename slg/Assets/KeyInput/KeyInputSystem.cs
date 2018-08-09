8f4363d46265056b530bcdcda1b3e06cd6f8cde1
saber <fatestaymq2@foxmail.com>
2018-08-09 19:07:57 +0800

    1. 视图移动以及旋转

---
+++ slg/Assets/KeyInput/KeyInputSystem.cs
@@ -0,0 +1,57 @@
+﻿using slg.controler;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public class KeyInputSystem : ComponentSystem
+{
+    private EntityManager em = World.Active.GetOrCreateManager<EntityManager>();
+    private float speed = 0.3f;
+    private bool isOverlook = true;
+    protected override void OnUpdate()
+    {
+        if (Input.GetKey("down"))
+        {
+            Camera.main.transform.Translate(new Vector3(0, -1* speed, 0));
+        }
+        if (Input.GetKey("up"))
+        {
+            Camera.main.transform.Translate(new Vector3(0, 1 * speed, 0));
+        }
+        if (Input.GetKey("left"))
+        {
+            Camera.main.transform.Translate(new Vector3(-1 * speed, 0, 0));
+        }
+        if (Input.GetKey("right"))
+        {
+            Camera.main.transform.Translate(new Vector3(1 * speed, 0, 0));
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (Camera.main.fieldOfView <= 100)
+                Camera.main.fieldOfView += 2;
+            if (Camera.main.orthographicSize <= 20)
+                Camera.main.orthographicSize += 0.5F;
+        }
+        //Zoom in
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (Camera.main.fieldOfView > 2)
+                Camera.main.fieldOfView -= 2;
+            if (Camera.main.orthographicSize >= 1)
+                Camera.main.orthographicSize -= 0.5F;
+        }
+
+        if (!isOverlook && Input.GetKeyDown("q"))
+        {
+            Camera.main.transform.Rotate(45,0,0);
+            isOverlook = true;
+        }
+        if (isOverlook && Input.GetKeyDown("w"))
+        {
+            Camera.main.transform.Rotate(-45,0,0);
+            isOverlook = false;
+        }
+
+    }
+}
--- slg/Assets/Main.cs
+++ slg/Assets/Main.cs
@@ -4,11 +4,12 @@ public class Main : MonoBehaviour
 {
     void Start()
     {
+        Camera.main.transform.position = new Vector3(12, 15, 7);
     }
 
     private void Update()
     {
-       
+      
     }
 
 }
